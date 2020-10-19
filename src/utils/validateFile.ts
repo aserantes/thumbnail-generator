68af7042fb9e1e11abef1e0bfafa634e1514404b
Alejandro Serantes <alejandro.serantes@globant.com>
2020-10-19 03:47:32 -0300

    perfect, before craco

--- src/utils/validateFile.ts
+++ src/utils/validateFile.ts
@@ -12,17 +12,23 @@ interface FileInfo {
 
 export const validateFile = (fileInfo: FileInfo) => {
   const { size, type } = fileInfo;
-  let isValidType = false
+  let isValidType = false;
   const errors: string[] = [];
 
   VALID_FILE_TYPES.forEach((validType) => {
-    if (!isValidType){
-      if (validType.includes(type)){
-        isValidType = true
+    if (!isValidType) {
+      if (validType.includes(type)) {
+        isValidType = true;
       }
     }
   });
+
+  const lala = "lala".includes("la");
+
+  console.log(lala);
+
   if (size > MAX_FILE_SIZE) errors.push(MAX_FILE_SIZE_ERROR);
+
   if (VALID_FILE_TYPES.includes(type)) errors.push(VALID_FILE_TYPES_ERROR);
 
   return errors;
