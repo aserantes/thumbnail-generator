4f61ae3edc2e710b9dafa340550d70a0d820aa30
Alejandro Serantes <alejandro.serantes@globant.com>
2020-10-25 23:42:06 -0300

    it was a git folder case problem. should be fixed.

---
+++ src/hooks/useFileTypeAnalizer.tsx
@@ -0,0 +1,26 @@
+import FileType from "file-type";
+import { useEffect, useState } from "react";
+
+export function useFileTypeAnalizer(path: string) {
+  const [result, setResult] = useState({ ext: "", mime: "" });
+
+  useEffect(() => {
+    async function getFileInfo() {
+      let response;
+      if (path) {
+        const blob = await fetch(path).then((res) => res.blob());
+        const buffer = await blob.arrayBuffer().then((res) => res);
+
+        response = await FileType.fromBuffer(buffer);
+      }
+
+      response && setResult({ ext: response.ext, mime: response.mime });
+    }
+
+    if (path) {
+      getFileInfo();
+    }
+  }, [path]);
+
+  return [result];
+}
---
+++ src/styles/styled.d.ts
@@ -0,0 +1,7 @@
+import { ThemeType } from "./";
+import {} from "styled-components";
+
+declare module "styled-components" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends ThemeType {} // extends the global DefaultTheme with our ThemeType.
+}
